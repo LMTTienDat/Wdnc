64bca647afbd63a5c2e3841eb1a124dda04a1a97
TienDat <mattyboy812@gmail.com>
2023-03-01 19:01:24 +0700

    commit1

---
+++ src/TipsAndTricks/TatBlog.Core/Constants/IPagingParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Core.Constants;
+
+public interface IPagingParams
+{
+	int PageSize { get; set; }
+	int PageNumber { get; set; }
+	int SortColumn { get; set; }
+	int SortOrder { get; set; }
+	
+}

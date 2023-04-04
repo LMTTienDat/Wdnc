c668c4d573e563e95ebf176bd409bbee1d99a860
TienDat <95626306+LMTTienDat@users.noreply.github.com>
2023-04-04 15:27:45 +0700

    Lab05

--- TAT-BLOG/src/Components/Sidebar.js
+++ TAT-BLOG/src/Components/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import CategoriesWidget from "./CategoriesWidget";
+import SearchForm from "./SearchForm";
 
 const Sidebar = () => {
     return (
         <div className="pt-4 ps-2">
+            <SearchForm/>
             <CategoriesWidget/>
 
             <h1>
--- TAT-BLOG/src/Services/Widgets.js
+++ TAT-BLOG/src/Services/Widgets.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getCategories() {
     try {
         const response = await
-axios.get('https://localhost:7085/api/categories');
+axios.get('https://localhost:7281/api/categories');
     const data = response.data;
     if (data.isSuccess)
         return data.result;
